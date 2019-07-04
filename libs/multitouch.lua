e1c9a177dbf5c383703c65fd4a820cc71e16019f
shieldax <Shieldax@gmail.com>
2019-07-04 18:38:35 +0800

    Backup multitouch lib

--- libs/multitouch.lua
+++ libs/multitouch.lua
@@ -1,4 +1,50 @@
-function calculateDelta( previousTouches, event )
+--
+-- Project: PinchZoom
+--
+-- Date: August 19, 2010
+--
+-- Version: 1.1
+--
+-- File name: main.lua
+--
+-- Author: Corona Labs
+--
+-- Abstract: Pinch/Zoom Gesture sample app
+--
+-- Demonstrates: locations events, buttons, touch
+--
+-- File dependencies: none
+--
+-- Target devices: Devices only
+--
+-- Limitations: Mutitouch not supported on Simulator
+--
+-- Update History:
+--	v1.1	Added Simulator warning message
+--
+-- Comments: Pinch and Zoom to scale the image on the screen.
+--
+-- Sample code is MIT licensed, see https://www.coronalabs.com/links/code/license
+-- Copyright (C) 2010 Corona Labs Inc. All Rights Reserved.
+--
+-- Supports Graphics 2.0
+---------------------------------------------------------------------------------------
+--[[
+-- add bkgd image to screen
+local background = display.newImage( "aquariumbackgroundIPhone.jpg", display.contentCenterX, display.contentCenterY )
+
+if not system.hasEventSource( "multitouch" ) then
+	local msg = display.newText( "Multitouch events not supported on this platform", 0, 20, native.systemFontBold, 13 )
+	msg.x = display.contentWidth / 2
+	msg:setFillColor( 1, 0, 0 )
+else
+	-- activate multitouch
+	system.activate( "multitouch" )
+	local msg = display.newText( "Pinch and zoom the image", display.contentWidth / 2, display.contentHeight - 40, native.systemFontBold, 13 )
+	msg:setFillColor( 0, 1, 0 )
+end
+
+local function calculateDelta( previousTouches, event )
 	local id,touch = next( previousTouches )
 	if event.id == id then
 		id,touch = next( previousTouches, id )
@@ -7,194 +53,279 @@ function calculateDelta( previousTouches, event )
 
 	local dx = touch.x - event.x
 	local dy = touch.y - event.y
-
-	midX=math.abs((touch.x + event.x)/2)
-	midY=math.abs((touch.y + event.y)/2)
-	
-	offX=imageContainer.x-midX
-	offY=imageContainer.y-midY
-	
-	return dx, dy,midX,midY,offX,offY
+	return dx, dy
 end
 
-function loadImage()
+-- create a table listener object for the bkgd image
+function background:touch( event )
+	local result = true
 
-	system.activate( "multitouch" )
-	imageContainer=display.newContainer(2048,1536)
-		
-	chartImage=display.newImageRect("photo.jpg",2048,1536)	
-	imageContainer:insert(chartImage)
-	imageContainer:addEventListener( "touch", imageContainer )
-	
-	imageContainer.x=512
-	imageContainer.y=386
-	imageContainer.anchorChildren = true
-	prevX=imageContainer.x
-	prevY=imageContainer.y
-	
-	isMulti=false
+	local phase = event.phase
 
-	function imageContainer:touch( event )
-		display.remove(buttonText)
-		local scale
-		local result = true
-			local phase = event.phase
-			local previousTouches = self.previousTouches
+	local previousTouches = self.previousTouches
 
-			local numTotalTouches = 1
-			if ( previousTouches ) then
-				-- add in total from previousTouches, subtract one if event is already in the array
-				numTotalTouches = numTotalTouches + self.numPreviousTouches
-				if previousTouches[event.id] then
-					numTotalTouches = numTotalTouches - 1
-				end
+	local numTotalTouches = 1
+	if ( previousTouches ) then
+		-- add in total from previousTouches, subtract one if event is already in the array
+		numTotalTouches = numTotalTouches + self.numPreviousTouches
+		if previousTouches[event.id] then
+			numTotalTouches = numTotalTouches - 1
+		end
+	end
+
+	if "began" == phase then
+		-- Very first "began" event
+		if ( not self.isFocus ) then
+			-- Subsequent touch events will target button even if they are outside the contentBounds of button
+			display.getCurrentStage():setFocus( self )
+			self.isFocus = true
+
+			previousTouches = {}
+			self.previousTouches = previousTouches
+			self.numPreviousTouches = 0
+		elseif ( not self.distance ) then
+			local dx,dy
+
+			if previousTouches and ( numTotalTouches ) >= 2 then
+				dx,dy = calculateDelta( previousTouches, event )
 			end
 
-			if "began" == phase then
-				-- Very first "began" event
-				imageContainer.xScaleOriginal=imageContainer.xScale
-				if ( not self.isFocus ) then
-					-- Subsequent touch events will target button even if they are outside the contentBounds of button
-					display.getCurrentStage():setFocus( self )
-					self.isFocus = true
-					previousTouches = {}
-					self.previousTouches = previousTouches
-					self.numPreviousTouches = 0
-					----ADDED BY CON TO ORIGINAL PINCHZOOM CODE
-					startX=event.x
-					startY=event.y
-					--------------------------------
-					
-				elseif ( not self.distance ) then
-					local dx,dy
-					isMulti=true
-					if previousTouches and ( numTotalTouches ) >= 2 then
-						
-						dx,dy,midX,midY,offX,offY = calculateDelta( previousTouches, event )
-					end
-					-- initialize to distance between two touches
-					if ( dx and dy ) then
-						local d = math.sqrt( dx*dx + dy*dy )
-						if ( d > 0 ) then
-							
-							imageContainer.distance = d
-							offCX=(imageContainer.width*imageContainer.xScale/2)-imageContainer.x
-							offCY=(imageContainer.height*imageContainer.yScale/2)-imageContainer.y
-							local newAnchorY=(midY+offCY)/(imageContainer.height*imageContainer.yScale)
-							local newAnchorX=(midX+offCX)/(imageContainer.width*imageContainer.xScale)
-							
-							imageContainer.anchorX=newAnchorX
-							imageContainer.anchorY=newAnchorY							
-							imageContainer.x=imageContainer.x-offX
-							imageContainer.y=imageContainer.y-offY
-	
-						end
-					end
-				end
-				
-				if not previousTouches[event.id] then
-					self.numPreviousTouches = self.numPreviousTouches + 1
-				end
-				previousTouches[event.id] = event
-
-			elseif self.isFocus then
-				if "moved" == phase then
-					if ( imageContainer.distance ) then
-						local dx,dy
-						if previousTouches and ( numTotalTouches ) >= 2 then
-							dx,dy = calculateDelta( previousTouches, event )
-						end
-			
-						if ( dx and dy ) then
-							local newDistance = math.sqrt( dx*dx + dy*dy )
-							modScale = newDistance / imageContainer.distance
-							if ( modScale > 0 ) then
-								----MODIFIED BY CON
-								local newScale=imageContainer.xScaleOriginal * modScale
-								-- uncomment below to set max and min scales
-								maxScale,minScale=2,0.2
-								if (newScale>maxScale) then 
-									newScale=maxScale 
-								end
-								if (newScale<minScale) then 
-									newScale=minScale 
-								end
-
-								imageContainer.xScale = newScale
-								imageContainer.yScale = newScale
-								-----------------------------
-							end
-						end
-					----ADDED BY CON TO ORIGINAL PINCHZOOM CODE
-
-					else
-							local deltaX = prevX+event.x - startX
-							local deltaY = prevY+event.y - startY
-							imageContainer.x = deltaX
-							imageContainer.y = deltaY	
-							--limits on image movement													
-							local limit=300
-							local bounds = imageContainer.contentBounds 
-							if (bounds.xMin>512+limit) then imageContainer.x=imageContainer.x-100 end
-							if (bounds.yMin>376+limit) then imageContainer.y=imageContainer.y-100 end							
-							if (bounds.xMax<512-limit) then imageContainer.x=imageContainer.x+100 end
-							if (bounds.yMax<376-limit) then imageContainer.y=imageContainer.y+100 end
-					---------------------------------
-					end
-
-					if not previousTouches[event.id] then
-						self.numPreviousTouches = self.numPreviousTouches + 1
-					end
-					previousTouches[event.id] = event
-
-				elseif "ended" == phase or "cancelled" == phase then									
-					
-					if previousTouches[event.id] then
-						self.numPreviousTouches = self.numPreviousTouches - 1
-						previousTouches[event.id] = nil
-					end
-					if (imageContainer.anchorX~=0.5) then
-						axOff=imageContainer.anchorX-0.5
-						ayOff=imageContainer.anchorY-0.5
-						imageContainer.anchorX=0.5
-						imageContainer.anchorY=0.5
-
-						imageContainer.x=imageContainer.x-(axOff*(imageContainer.width*imageContainer.xScale))
-						imageContainer.y=imageContainer.y-(ayOff*(imageContainer.height*imageContainer.yScale))
-
-					end
-					if ( #previousTouches > 0 ) then
-						-- must be at least 2 touches remaining to pinch/zoom
-						self.distance = nil
-
-					else
-						-- previousTouches is empty so no more fingers are touching the screen
-						-- Allow touch events to be sent normally to the objects they "hit"
-						display.getCurrentStage():setFocus( nil )
-						self.isFocus = false
-						self.distance = nil
-						-- reset array
-						self.previousTouches = nil
-						self.numPreviousTouches = nil
-						
-						----ADDED BY CON TO ORIGINAL PINCHZOOM CODE
-						-- Detecting a touch event or move event that's less than 12 pixels as a selection within the image
-						if (math.abs(startX-event.x)<12) and (math.abs(startY-event.y)<12) and (not isMulti) then submitTouch(event.x,event.y) end
-						prevX = imageContainer.x
-						prevY = imageContainer.y
-
-						----------------------------
-					end
+			-- initialize to distance between two touches
+			if ( dx and dy ) then
+				local d = math.sqrt( dx*dx + dy*dy )
+				if ( d > 0 ) then
+					self.distance = d
+					self.xScaleOriginal = self.xScale
+					self.yScaleOriginal = self.yScale
+					print( "distance = " .. self.distance )
 				end
 			end
 		end
-		isMulti=false
-		return result
 
+		if not previousTouches[event.id] then
+			self.numPreviousTouches = self.numPreviousTouches + 1
+		end
+		previousTouches[event.id] = event
+
+	elseif self.isFocus then
+		if "moved" == phase then
+			if ( self.distance ) then
+				local dx,dy
+				if previousTouches and ( numTotalTouches ) >= 2 then
+					dx,dy = calculateDelta( previousTouches, event )
+				end
+	
+				if ( dx and dy ) then
+					local newDistance = math.sqrt( dx*dx + dy*dy )
+					local scale = newDistance / self.distance
+					print( "newDistance(" ..newDistance .. ") / distance(" .. self.distance .. ") = scale("..  scale ..")" )
+					if ( scale > 0 ) then
+						self.xScale = self.xScaleOriginal * scale
+						self.yScale = self.yScaleOriginal * scale
+					end
+				end
+			end
+
+			if not previousTouches[event.id] then
+				self.numPreviousTouches = self.numPreviousTouches + 1
+			end
+			previousTouches[event.id] = event
+
+		elseif "ended" == phase or "cancelled" == phase then
+			if previousTouches[event.id] then
+				self.numPreviousTouches = self.numPreviousTouches - 1
+				previousTouches[event.id] = nil
+			end
+
+			if ( #previousTouches > 0 ) then
+				-- must be at least 2 touches remaining to pinch/zoom
+				self.distance = nil
+			else
+				-- previousTouches is empty so no more fingers are touching the screen
+				-- Allow touch events to be sent normally to the objects they "hit"
+				display.getCurrentStage():setFocus( nil )
+
+				self.isFocus = false
+				self.distance = nil
+				self.xScaleOriginal = nil
+				self.yScaleOriginal = nil
+
+				-- reset array
+				self.previousTouches = nil
+				self.numPreviousTouches = nil
+			end
+		end
+	end
+
+	return result
 end
 
-function submitTouch(x,y)
-	--use this for a single touch event
+--
+-- register table listener
+background:addEventListener( "touch", background )
+--]]
+--- --------------------
+--- multitouch.lua
+local class = require 'libs.middleclass'
+
+local util = require 'util'
+local d = util.print_r
+
+local function isMultiTouchable()
+  if not system.hasEventSource( "multitouch" ) then
+--    local msg = display.newText( "Multitouch events not supported on this platform", 0, 20, native.systemFontBold, 13 )
+--    msg.x = display.contentWidth / 2
+--    msg:setFillColor( 1, 0, 0 )
+    return false
+  else
+    -- activate multitouch
+    system.activate( "multitouch" )
+--    local msg = display.newText( "Pinch and zoom the image", display.contentWidth / 2, display.contentHeight - 40, native.systemFontBold, 13 )
+--    msg:setFillColor( 0, 1, 0 )
+    return true
+  end
 end
 
-loadImage()
+local function calculateDelta( previousTouches, event )
+	local id,touch = next( previousTouches )
+	if event.id == id then
+		id,touch = next( previousTouches, id )
+		assert( id ~= event.id )
+	end
+
+	local dx = touch.x - event.x
+	local dy = touch.y - event.y
+	return dx, dy
+end
+-- ----------------------------------------
+-- ----------------------------------------
+-- Pinch Zoom Gesture Class as [Mixin Class]
+-- TargetClass:include(PinchZoomGesture)
+--
+local PinchZoomGesture = class "PinchZoomGesture"
+
+function PinchZoomGesture:initialize()
+  if isMultiTouchable() then
+    d('Multitouch Actived -- Pinch and zoom the image')
+  else
+    d('Multitouch events not supported on this platform')
+  end
+end
+
+function PinchZoomGesture:touch(event)
+  local result = true
+
+	local phase = event.phase
+
+	local previousTouches = self.previousTouches
+
+	local numTotalTouches = 1
+	if ( previousTouches ) then
+		-- add in total from previousTouches, subtract one if event is already in the array
+		numTotalTouches = numTotalTouches + self.numPreviousTouches
+		if previousTouches[event.id] then
+			numTotalTouches = numTotalTouches - 1
+		end
+	end
+
+	if "began" == phase then
+		-- Very first "began" event
+		if ( not self.isFocus ) then
+			-- Subsequent touch events will target button even if they are outside the contentBounds of button
+			display.getCurrentStage():setFocus( self )
+			self.isFocus = true
+
+			previousTouches = {}
+			self.previousTouches = previousTouches
+			self.numPreviousTouches = 0
+		elseif ( not self.distance ) then
+			local dx,dy
+
+			if previousTouches and ( numTotalTouches ) >= 2 then
+				dx,dy = calculateDelta( previousTouches, event )
+			end
+
+			-- initialize to distance between two touches
+			if ( dx and dy ) then
+				local d = math.sqrt( dx*dx + dy*dy )
+				if ( d > 0 ) then
+					self.distance = d
+					self.xScaleOriginal = self.xScale
+					self.yScaleOriginal = self.yScale
+					print( "distance = " .. self.distance )
+				end
+			end
+		end
+
+		if not previousTouches[event.id] then
+			self.numPreviousTouches = self.numPreviousTouches + 1
+		end
+		previousTouches[event.id] = event
+
+	elseif self.isFocus then
+		if "moved" == phase then
+			if ( self.distance ) then
+				local dx,dy
+				if previousTouches and ( numTotalTouches ) >= 2 then
+					dx,dy = calculateDelta( previousTouches, event )
+				end
+	
+				if ( dx and dy ) then
+					local newDistance = math.sqrt( dx*dx + dy*dy )
+					local scale = newDistance / self.distance
+					print( "newDistance(" ..newDistance .. ") / distance(" .. self.distance .. ") = scale("..  scale ..")" )
+					if ( scale > 0 ) then
+						self.xScale = self.xScaleOriginal * scale
+						self.yScale = self.yScaleOriginal * scale
+					end
+				end
+			end
+
+			if not previousTouches[event.id] then
+				self.numPreviousTouches = self.numPreviousTouches + 1
+			end
+			previousTouches[event.id] = event
+
+		elseif "ended" == phase or "cancelled" == phase then
+			if previousTouches[event.id] then
+				self.numPreviousTouches = self.numPreviousTouches - 1
+				previousTouches[event.id] = nil
+			end
+
+			if ( #previousTouches > 0 ) then
+				-- must be at least 2 touches remaining to pinch/zoom
+				self.distance = nil
+			else
+				-- previousTouches is empty so no more fingers are touching the screen
+				-- Allow touch events to be sent normally to the objects they "hit"
+				display.getCurrentStage():setFocus( nil )
+
+				self.isFocus = false
+				self.distance = nil
+				self.xScaleOriginal = nil
+				self.yScaleOriginal = nil
+
+				-- reset array
+				self.previousTouches = nil
+				self.numPreviousTouches = nil
+			end
+		end
+	end
+
+	return result
+end
+
+function PinchZoomGesture:activeOn(name)
+  if self[name] and util.isDisplayObject(self[name]) then
+    self[name]:addEventListener('touch', self)
+    return true
+  else
+    return false
+  end
+end
+
+function PinchZoomGesture:onInclude()
+  d('Multitouch Gesture Included')
+end
+
+return PinchZoomGesture
