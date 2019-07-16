626c6ce239ee3249ce1003227b6db22c16eda323
ShieldaX <shieldax@gmail.com>
2019-07-16 13:53:40 +0800

    Appbar: use group to wrap special rect

--- main.lua
+++ main.lua
@@ -32,7 +32,6 @@ local composer = require "composer"
 composer.gotoScene("scenes.app")
 --composer.gotoScene("scenes.album_list")
 
-
 local function panelTransDone( target )
   --native.showAlert( "Panel", "Complete", { "Okay" } )
   if ( target.completeState ) then
@@ -40,20 +39,52 @@ local function panelTransDone( target )
   end
 end
 
+-- Configure the tab buttons to appear within the bar
+local tabButtons = {
+  {
+    label = "Explore",
+    id = "tab_xplr",
+    selected = true,
+    labelYOffset = -8,
+    labelColor = { default={ 0, 0, 0, 0.4 }, over={colorHex('C7A680')} },
+    onPress = handleTabBarEvent
+  },
+  {
+    label = "Search",
+    id = "tab_search",
+    labelYOffset = -8,
+    labelColor = { default={ 0, 0, 0, 0.4 }, over={colorHex('C7A680')} }, 
+    onPress = handleTabBarEvent
+  },
+  {
+    label = "User",
+    id = "tab_mine",
+    labelYOffset = -8,
+    labelColor = { default={ 0, 0, 0, 0.4 }, over={colorHex('C7A680')} },
+    onPress = handleTabBarEvent
+  }
+}
+
 local panel = widget.newPanel{
   location = "bottom",
   onComplete = panelTransDone,
   width = display.contentWidth,
-  height = 62,
-  speed = 250,
+  height = 80,
+  speed = 420,
   inEasing = easing.outBack,
   outEasing = easing.outCubic
 }
-panel.background = display.newRoundedRect( 0, 0, panel.width, 62, 20 )
-panel.background:setFillColor(colorHex('1A1A19'))
+local backgroundRect = display.newRoundedRect( 0, 0, panel.width, 80, 36 )
+backgroundRect:setFillColor(colorHex('1A1A19'))
+local beyondRect = display.newRect(0, 40, panel.width, 40)
+beyondRect:setFillColor(colorHex('1A1A19'))
+panel.background = display.newGroup()
+panel.background.anchorChildren = true
+background:insert(beyondRect)
+background:insert(backgroundRect)
 panel:insert( panel.background )
 
-panel.title = display.newText( "menu", 0, 0, native.systemFontBold, 18 )
-panel.title:setFillColor( 1, 1, 1 )
+panel.title = display.newText( "Explore", 0, 0, native.systemFontBold, 20 )
+panel.title:setFillColor(colorHex('C7A680'))
 panel:insert( panel.title )
 timer.performWithDelay(800, function() panel:show() end)
