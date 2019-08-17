7c9fba881000bf3df1159ba6334accc44f1f97b2
ShieldaX <shieldax@gmail.com>
2019-08-17 09:14:17 +0800

    Application Class: manage scene by each tab

--- classes/application.lua
+++ classes/application.lua
@@ -7,30 +7,38 @@ local M = {}
 -- Gather insets (function returns these in the order of top, left, bottom, right)
 M.topInset, M.leftInset, M.bottomInset, M.rightInset = display.getSafeAreaInsets()
 
-local scenes = {}
+local scenes = {
+    home = {},
+    search = {},
+    mine = {}
+  }
 
-function M.pushScene(scene)
+function M.pushScene(scene, tab)
+  tab = tab or 'home'
   print(scene.name .. ' pushed to scene history')
-  return table.insert(scenes, scene)
+  return table.insert(scenes[tab], scene)
 end
 
-function M.popScene()
-  return table.remove(scenes)
+function M.popScene(tab)
+  tab = tab or 'home'
+  return table.remove(scenes[tab])
 end
 
 function M._scenes()
   d(scenes)
 end
 
-function M.currentScene(key, value)
+function M.currentScene(tab, key, value)
+  tab = tab or 'home'
+  local scenes = scenes[tab]
   local scene = scenes[#scenes]
   if key then scene[key] = value end
   return scene
 end
 
-function M.previousScene()
-  --local scene = table.remove(scenes)
-  --print(scene.name .. ' poped from scene history')
+function M.previousScene(tab)
+  tab = tab or 'home'
+  local scenes = scenes[tab]  
   return scenes[#scenes-1]
 end
 
@@ -38,12 +46,12 @@ function M:sceneForwards(params)
   local currentSceneName = composer.getSceneName('current')
   local sceneToRemove = composer.getVariable('sceneToRemove')
   if not sceneToRemove then
-    self.pushScene({name = currentSceneName, params = params})
+    self.pushScene({name = currentSceneName, params = params}, 'home')
   end
 end
 
-function M:sceneBackwards()
-  local prevScene = self.previousScene()
+function M:sceneBackwards(tab)
+  local prevScene = self.previousScene(tab)
   if prevScene then
     d('CBack to :')
     d(prevScene.name)
@@ -54,7 +62,7 @@ function M:sceneBackwards()
     else
       d('not remove '..currentSceneName)
     end
-    self.popScene()
+    self.popScene(tab)
     composer.gotoScene( prevScene.name, {effect = 'slideRight', time = 420, params = prevScene.params} )
   end
 end
